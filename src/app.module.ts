03a4fbbb1423681675f2a5ed0ad08701ab3d238f
Jaime Estuardo Sazo <estuardosazo1206@gmail.com>
2025-11-28 07:21:26 -0600

    feat: Initialize NestJS project with configuration, TypeORM for MySQL, and core user/auth modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_MYSQL_HOST,
+      port: Number(process.env.DB_MYSQL_PORT),
+      username: process.env.DB_MYSQL_USER,
+      password: process.env.DB_MYSQL_PASS,
+      database: process.env.DB_MYSQL_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
